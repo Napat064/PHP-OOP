1c2825af258642057b100b3ed6f0791d6651a252
Wuttha <Wuttha@webmail.npru.ac.th>
2026-07-09 11:02:20 +0700

    add Fruit class with properties and methods for setting and displaying details

---
+++ W1.php
@@ -0,0 +1,20 @@
+<?php
+class Fruit
+{
+    // คุณสมบัติ (Properties หรือตัวแปรภายในคลาส)
+    public $name;
+    public $color;
+
+    // เมธอดสำหรับกำหนดค่าให้กับคุณสมบัติ (Method to set the properties)
+    function set_details($name, $color)
+    {
+        $this->name = $name;
+        $this->color = $color;
+    }
+
+    // เมธอดสำหรับแสดงค่าของคุณสมบัติออกทางหน้าจอ (Method to display the properties)
+    function get_details()
+    {
+        echo "Name: " . $this->name . ". Color: " . $this->color . ".<br>";
+    }
+}
